e0d67e1fa740bd3cd708859ad6fecb040c7ba255
root <swapnilnagaonkar>
2019-06-15 06:03:57 +0000

    Code for async understaing

--- wheather-app/app.js
+++ wheather-app/app.js
@@ -1,7 +1,11 @@
 console.log('STARTING')
 
 setTimeout(() => {
-    console.log('HERERer');
+    console.log('2 seconds');
 },2000)
+
+setTimeout(() => {
+    console.log('0 seconds');
+},0)
 
 console.log('ENDING')
