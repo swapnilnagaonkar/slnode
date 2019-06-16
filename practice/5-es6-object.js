ed252646fda816e5ace7badbd2b721a8aadea072
swapnilnagaonkar <nagaonkarswapnil@gmail.com>
2019-06-16 11:08:13 +0530

    object property shorthand

--- practice/5-es6-object.js
+++ practice/5-es6-object.js
@@ -1,10 +1,12 @@
+// object property shorthand
 const name = 'Madhav'
 const userAge = '28'
 
 const user = {
-    name : name,
+    name,
     age : userAge,
     location : 'Mumbai'
 }
 
 console.log(user)
+
